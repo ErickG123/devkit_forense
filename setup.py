256437244e81754d8487d3eca403150a27bbd670
Erick Gabriel <erickgabrielalves@hotmail.com>
2025-09-03 09:10:37 -0300

    [patch] correção na geração da release e tag

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name="forensic_cli",
-    version="1.2.8",
-    author="Erick Gabriel dos Santos Alves",
+    version="1.2.11",
+    author="Erick Gabriel",
     author_email="erickgabrielalves0@gmail.com",
     description="Toolkit modular para análise de evidências digitais",
     long_description=open("README.md").read(),
